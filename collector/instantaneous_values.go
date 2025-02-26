017f29db8641280db6a7f39922ff7a21e1f66b22
DazWilkin <daz.wilkin@gmail.com>
2025-02-26 10:40:38 -0800

    Some refactoring and better error-handling

--- collector/instantaneous_values.go
+++ collector/instantaneous_values.go
@@ -1,6 +1,8 @@
 package collector
 
 import (
+	"errors"
+	"fmt"
 	"log/slog"
 	"strconv"
 
@@ -52,6 +54,15 @@ func (c *InstantaneousValuesCollector) Collect(ch chan<- prometheus.Metric) {
 		return
 	}
 
+	rqstMap := make(map[string]bool)
+
+	// Mark all requested sites as existing
+	for _, site := range c.Sites {
+		rqstMap[site] = true
+	}
+
+	respMap := make(map[string]bool)
+
 	// []TimeSeries contains more than just Gage Height measurements
 	// Must filter results to ensure the VariableCode[].Value only contains GageHeightFeet ("00065")
 	// TODO extend this filter if other Prometheus Metrics are added
@@ -59,6 +70,18 @@ func (c *InstantaneousValuesCollector) Collect(ch chan<- prometheus.Metric) {
 		logger.Info("Iterating over results",
 			"name", t.Name,
 		)
+
+		sitecode, err := c.getSitecode(t)
+		if err != nil {
+			logger.Info("Unable to get sitecode",
+				"name", t.Name,
+				"error", err,
+			)
+			continue
+		}
+
+		// Compare requested c.Sites and returned resp.Value.TimeSeries.SourceInfo.Sitecode
+		respMap[sitecode] = true
 
 		// Filter TimeSeries to only those where VariableCode[].Value contains GageHeightFeet
 		if !t.Variable.Contains(waterdata.GageHeightFeet) {
@@ -68,26 +91,37 @@ func (c *InstantaneousValuesCollector) Collect(ch chan<- prometheus.Metric) {
 			continue
 		}
 
+		value, err := c.getValue(t)
+		if err != nil {
+			logger.Info("Unable to get value",
+				"name", t.Name,
+				"error", err,
+			)
+			continue
+		}
+		logger.Info("Measured",
+			"name", t.Name,
+			"sitecode", sitecode,
+			"value", value,
+		)
+
 		// Only GageHeightFeet measurements are left
 		// TODO Check to corrorboate sites parameter with values returned
+
 		ch <- prometheus.MustNewConstMetric(
 			c.GageHeightFeet,
 			prometheus.GaugeValue,
-			func(v string) float64 {
-				r, err := strconv.ParseFloat(v, 64)
-				if err != nil {
-					logger.Info("Unable to parse value as float64",
-						"value", v,
-					)
-					return 0.0
-				}
-
-				return r
-			}(t.Values[0].Value[0].Value),
+			value,
 			[]string{
-				//TODO Why is it a slice of SiteCode?
-				t.SourceInfo.SiteCode[0].Value,
+				sitecode,
 			}...,
+		)
+	}
+
+	if notResp, notRqst := c.findMismatches(rqstMap, respMap); len(notResp) != 0 || len(notRqst) != 0 {
+		logger.Info("Sites not matched",
+			"requested but not responded", notResp,
+			"responded but not requested", notRqst,
 		)
 	}
 }
@@ -95,4 +129,98 @@ func (c *InstantaneousValuesCollector) Collect(ch chan<- prometheus.Metric) {
 // Describe is a method that implements Prometheus' Collector interface and describes metrics
 func (c *InstantaneousValuesCollector) Describe(ch chan<- *prometheus.Desc) {
 	ch <- c.GageHeightFeet
+}
+
+// getSitecode is a method that returns the sitecode from the TimeSeries
+func (c *InstantaneousValuesCollector) getSitecode(t waterdata.TimeSeries) (string, error) {
+	logger := c.Logger.With("method", "getSitecode")
+
+	if len(t.SourceInfo.SiteCode) == 0 {
+		msg := "no site code returned"
+		logger.Info(msg,
+			"name", t.Name,
+		)
+		return "", errors.New(msg)
+	}
+
+	logger.Info("Sitecodes returned",
+		"name", t.Name,
+		"sitecodes", len(t.SourceInfo.SiteCode),
+	)
+
+	sitecode := t.SourceInfo.SiteCode[0].Value
+
+	return sitecode, nil
+}
+
+// getValue is a method that returns the value from the TimeSeries
+// It only returns the first value in the Values array
+func (c *InstantaneousValuesCollector) getValue(t waterdata.TimeSeries) (float64, error) {
+	logger := c.Logger.With("method", "getValue")
+
+	if len(t.Values) == 0 {
+		msg := "no values returned"
+		logger.Info(msg,
+			"name", t.Name,
+		)
+		return 0.0, errors.New(msg)
+	}
+
+	logger.Info("Values returned",
+		"name", t.Name,
+		"values", len(t.Values),
+	)
+
+	if len(t.Values[0].Value) == 0 {
+		msg := "no Value[0] values returned"
+		logger.Info(msg,
+			"name", t.Name,
+			"values", t.Values[0].Value,
+		)
+		return 0.0, errors.New(msg)
+	}
+
+	logger.Info("Values[0] values (!) returned",
+		"name", t.Name,
+		"values", len(t.Values[0].Value),
+	)
+
+	v := t.Values[0].Value[0].Value
+	value, err := strconv.ParseFloat(v, 64)
+	if err != nil {
+		msg := fmt.Sprintf("unable to parse value (%s) as float64", v)
+		logger.Info(msg,
+			"value", v,
+		)
+		return 0.0, errors.New(msg)
+	}
+
+	return value, nil
+
+}
+
+// findMismatches is a method that returns the sites that are not matched
+// It returns two slices, one for sites that were requested but not responded
+// and one for sites that were responded but not requested
+func (c *InstantaneousValuesCollector) findMismatches(rqstMap, respMap map[string]bool) ([]string, []string) {
+	// logger := c.Logger.With("method", "findMismatches")
+
+	// Iterate over maps determining which sites are missing
+	// Requested but not responded
+	notResp := []string{}
+	for site := range rqstMap {
+		if _, ok := respMap[site]; !ok {
+			notResp = append(notResp, site)
+		}
+	}
+
+	// Responded but not requested
+	notRqst := []string{}
+	for site := range respMap {
+		if _, ok := rqstMap[site]; !ok {
+			notRqst = append(notRqst, site)
+		}
+	}
+
+	return notResp, notRqst
 }
